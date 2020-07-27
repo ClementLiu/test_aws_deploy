f32d33969294a32757a22bd4fdef6fd0b09bb0b5
Clement <ziwei.liu@cca.edu>
2020-07-26 23:15:00 -0700

    test with json

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8080;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  console.log("req", req);
+  res.send(req.body);
+});
+app.get("/people.json", function (request, response) {
+  // We want to set the content-type header so that the browser understands
+  //  the content of the response.
+  response.contentType("application/json");
+
+  // Normally, the data is fetched from a database, but we can cheat:
+  var people = [
+    { name: "Dave", location: "Atlanta" },
+    { name: "Santa Claus", location: "North Pole" },
+    { name: "Man in the Moon", location: "The Moon" },
+  ];
+
+  // Since the request is for a JSON representation of the people, we
+  //  should JSON serialize them. The built-in JSON.stringify() function
+  //  does that.
+  var peopleJSON = JSON.stringify(people);
+
+  // Now, we can use the response object's send method to push that string
+  //  of people JSON back to the browser in response to this request:
+  // response.send(peopleJSON);
+  response.send(people);
+});
 
 app.listen(port, () =>
   console.log(
